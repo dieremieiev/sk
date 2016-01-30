c8568a3943667f60ae165bb9bb9b57fcbf043f4c
tvvlab <ch882010@gmail.com>
2016-01-30 23:29:13 +0100

    Use ng.router.HashLocationStrategy

--- src/main/webapp/app/boot.js
+++ src/main/webapp/app/boot.js
@@ -1,6 +1,11 @@
 (function(app) {
   document.addEventListener('DOMContentLoaded', function() {
     ng.core.enableProdMode()
-    ng.platform.browser.bootstrap(app.AppComponent, ng.router.ROUTER_PROVIDERS)
+    ng.platform.browser.bootstrap(
+      app.AppComponent,
+      [ng.router.ROUTER_PROVIDERS, ng.core.provide(ng.router.LocationStrategy, {
+        useClass: ng.router.HashLocationStrategy
+      })]
+    )
   })
 })(window.app || (window.app = {}))
