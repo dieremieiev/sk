1122bdba293bb971c3aca89b6197519d7dae12ca
tvvlab <ch882010@gmail.com>
2016-01-30 22:31:22 +0100

    Use https://code.angularjs.org/2.0.0-beta.2/

--- app/boot.js
+++ app/boot.js
@@ -1,6 +1,6 @@
 (function(app) {
   document.addEventListener('DOMContentLoaded', function() {
-   //ng.core.enableProdMode()
+    ng.core.enableProdMode()
     ng.platform.browser.bootstrap(app.AppComponent, ng.router.ROUTER_PROVIDERS)
   })
 })(window.app || (window.app = {}))
