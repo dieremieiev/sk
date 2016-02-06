a1fe8e77ce968f22dd287a8f49ad87552c55854c
tvvlab <ch882010@gmail.com>
2016-02-06 22:32:32 +0100

    Add new components: URI encoder/decoder, Timestamp and UUID generator

--- src/main/webapp/app/app.component.js
+++ src/main/webapp/app/app.component.js
@@ -12,6 +12,10 @@ function createAppComponent()
             <ul>
               <li><a [routerLink]="['Base64Decoder']">Base64 decoder</a></li>
               <li><a [routerLink]="['Base64Encoder']">Base64 encoder</a></li>
+              <li><a [routerLink]="['Timestamp']">Timestamp</a></li>
+              <li><a [routerLink]="['URIDecoder']">URI decoder</a></li>
+              <li><a [routerLink]="['URIEncoder']">URI encoder</a></li>
+              <li><a [routerLink]="['UUIDGenerator']">UUID generator</a></li>
             </ul>
           </nav>
         </div>
@@ -41,28 +45,39 @@ function createBase64DecoderComponent()
       </p>
       <p>
         <label>Decoded text:</label>
-        <textarea cols="40" rows="5" readonly="readonly">{{evaluate()}}</textarea>
+        <textarea cols="40" rows="5" readonly="readonly" placeholder="{{message}}">{{result}}</textarea>
       </p>
     `
   })
   .Class({
     constructor: function() {
-      this.input = null
+      this.input    = null
+      this.inputOld = null
+      this.message  = null
+      this.result   = null
     },
 
-    ngAfterViewInit: function() {
+    ngAfterContentChecked() { 
+      if (this.input != this.inputOld) {
+        this.evaluate()
+        this.inputOld = this.input
+      }
+    },
+
+    ngAfterViewInit() {
       this.vc.first.nativeElement.focus();
     },
 
     evaluate() {
-      if (this.input == null) {
-        return null
-      }
+      this.message = null
+      this.result  = null
+
+      if (this.input == null || this.input.length == 0) { return }
 
       try {
-        return atob(this.input)
+        this.result = atob(this.input)
       } catch (e) {
-        return 'Input string is not Base64 decoded'
+        this.message = 'Input string is not Base64 decoded'
       }
     }
   })
@@ -78,46 +93,267 @@ function createBase64EncoderComponent()
     template: `
       <h2>Base64 encoder</h2>
       <p>
-        <label>Text:</label>
+        <label>Input text:</label>
         <textarea #inputelement cols="40" rows="5" [(ngModel)]="input"></textarea>
       </p>
       <p>
         <label>Base64 encoded text:</label>
-        <textarea cols="40" rows="5" readonly="readonly">{{evaluate()}}</textarea>
+        <textarea cols="40" rows="5" readonly="readonly" placeholder="{{message}}">{{result}}</textarea>
       </p>
     `
   })
   .Class({
     constructor: function() {
-      this.input = null
+      this.input    = null
+      this.inputOld = null
+      this.message  = null
+      this.result   = null
     },
 
-    ngAfterViewInit: function() {
+    ngAfterContentChecked() {
+      if (this.input != this.inputOld) {
+        this.evaluate()
+        this.inputOld = this.input
+      }
+    },
+
+    ngAfterViewInit() {
       this.vc.first.nativeElement.focus();
     },
 
     evaluate() {
-      if (this.input == null) {
-        return null
-      }
+      this.message = null
+      this.result  = null
 
-      try {
-        return btoa(this.input)
+      if (this.input == null || this.input.length == 0) { return }
+
+      try { 
+        this.result = btoa(this.input)
       } catch (e) {
-        return 'Input string is not Base64 encoded'
+        this.message = 'Input string is not Base64 decoded'
       }
     }
   })
 }
 
+function createTimestampComponent()
+{
+  return ng.core.Component({
+    changeDetection: ng.core.ChangeDetectionStrategy.OnPush,
+    queries: {
+        vc: new ng.core.ViewChildren('inputelement')
+    },
+    template: `
+      <h2>Base64 encoder</h2>
+      <p>
+        <label>Timestamp, in milliseconds:</label>
+        <textarea #inputelement cols="40" rows="5" [(ngModel)]="input"></textarea>
+      </p>
+      <p>
+        <label>Date/time (ISO):</label>
+        <textarea cols="40" rows="5" readonly="readonly" placeholder="{{message}}">{{result}}</textarea>
+      </p>
+    `
+  })
+  .Class({
+    constructor: function() {
+      this.input    = null
+      this.inputOld = null
+      this.message  = null
+      this.result   = null
+    },
+
+    ngAfterContentChecked() {
+      if (this.input != this.inputOld) {
+        this.evaluate()
+        this.inputOld = this.input
+      }
+    },
+
+    ngAfterViewInit() {
+      this.vc.first.nativeElement.focus();
+    },
+
+    evaluate() {
+      this.message = null
+      this.result  = null
+
+      if (this.input == null || this.input.length == 0) { return }
+
+      var i = parseInt(this.input, 10)
+      var n = Number(this.input)
+      var d = i == n ? new Date(i) : null
+
+      if (d != null && !isNaN(d.getTime())) {
+        this.result = d.toISOString()
+      } else {
+        this.message = 'Wrong input value'
+      }
+    }
+  })
+}
+
+function createURIDecoderComponent()
+{
+  return ng.core.Component({
+    changeDetection: ng.core.ChangeDetectionStrategy.OnPush,
+    queries: {
+        vc: new ng.core.ViewChildren('inputelement')
+    },
+    template: `
+      <h2>URI decoder</h2>
+      <p>
+        <label>URI encoded:</label>
+        <textarea #inputelement cols="40" rows="5" [(ngModel)]="input"></textarea>
+      </p>
+      <p>
+        <label>URI decoded:</label>
+        <textarea cols="40" rows="5" readonly="readonly" placeholder="{{message}}">{{result}}</textarea>
+      </p>
+    `
+  })
+  .Class({
+    constructor: function() {
+      this.input    = null
+      this.inputOld = null
+      this.message  = null
+      this.result   = null
+    },
+
+    ngAfterContentChecked() {
+      if (this.input != this.inputOld) {
+        this.evaluate()
+        this.inputOld = this.input
+      }
+    },
+
+    ngAfterViewInit() {
+      this.vc.first.nativeElement.focus();
+    },
+
+    evaluate() {
+      this.message = null
+      this.result  = null
+
+      if (this.input == null || this.input.length == 0) { return }
+
+      try {
+        this.result = decodeURIComponent(this.input)
+      } catch (e) {
+        this.message = 'Error: ' + e.message
+      }
+    }
+  })
+}
+
+function createURIEncoderComponent()
+{
+  return ng.core.Component({
+    changeDetection: ng.core.ChangeDetectionStrategy.OnPush,
+    queries: {
+        vc: new ng.core.ViewChildren('inputelement')
+    },
+    template: `
+      <h2>URI encoder</h2>
+      <p>
+        <label>URI decoded:</label>
+        <textarea #inputelement cols="40" rows="5" [(ngModel)]="input"></textarea>
+      </p>
+      <p>
+        <label>URI encoded:</label>
+        <textarea cols="40" rows="5" readonly="readonly" placeholder="{{message}}">{{result}}</textarea>
+      </p>
+    `
+  })
+  .Class({
+    constructor: function() {
+      this.input    = null
+      this.inputOld = null
+      this.message  = null
+      this.result   = null
+    },
+
+    ngAfterContentChecked() {
+      if (this.input != this.inputOld) {
+        this.evaluate()
+        this.inputOld = this.input
+      }
+    },
+
+    ngAfterViewInit() {
+      this.vc.first.nativeElement.focus();
+    },
+
+    evaluate() {
+      this.message = null
+      this.result  = null
+
+      if (this.input == null || this.input.length == 0) { return }
+
+      try {
+        this.result = encodeURIComponent(this.input)
+      } catch (e) {
+        this.message = 'Error: ' + e.message
+      }
+    }
+  })
+}
+
+function createUUIDGeneratorComponent()
+{
+  return ng.core.Component({
+    changeDetection: ng.core.ChangeDetectionStrategy.OnPush,
+    queries: {
+        vc: new ng.core.ViewChildren('button')
+    },
+    template: `
+      <h2>UUID generator</h2>
+      <p>
+        <input #button type="button" value="Generate" (click)="evaluate()">
+      </p>
+      <p>
+        <label>UUID:</label>
+        <textarea cols="40" rows="5" readonly="readonly">{{result}}</textarea>
+      </p>
+    `
+  })
+  .Class({
+    constructor: function() {
+      this.result = null
+      this.evaluate()
+    },
+
+    ngAfterViewInit() {
+      this.vc.first.nativeElement.focus();
+    },
+
+    evaluate() {
+      this.result = this.generateUUID()
+    },
+
+    generateUUID() {
+      var d = new Date().getTime()
+
+      if (window.performance && typeof window.performance.now === 'function') {
+        d += performance.now()
+      }
+
+      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (d + Math.random()*16) % 16 | 0
+        d = Math.floor(d/16)
+        return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16)
+      })
+    }
+  })
+}
+
 (function(app) {
-  app.Base64DecoderComponent = createBase64DecoderComponent()
-  app.Base64EncoderComponent = createBase64EncoderComponent()
-
-  app.AppComponent = createAppComponent()
-
   app.AppComponent = ng.router.RouteConfig([
-    { path: '/base64decoder', component: app.Base64DecoderComponent, name:'Base64Decoder' },
-    { path: '/base64encoder', component: app.Base64EncoderComponent, name:'Base64Encoder' }
-  ])(app.AppComponent);
+    { path: '/base64decoder', component: createBase64DecoderComponent(), name: 'Base64Decoder' },
+    { path: '/base64encoder', component: createBase64EncoderComponent(), name: 'Base64Encoder' },
+    { path: '/timestamp'    , component: createTimestampComponent()    , name: 'Timestamp'     },
+    { path: '/uriencoder'   , component: createURIEncoderComponent()   , name: 'URIEncoder'    },
+    { path: '/uridecoder'   , component: createURIDecoderComponent()   , name: 'URIDecoder'    },
+    { path: '/uuidgenerator', component: createUUIDGeneratorComponent(), name: 'UUIDGenerator' }
+  ])(createAppComponent());
 })(window.app || (window.app = {}))
